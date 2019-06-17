0de36eedb371cc045fbf0c18a58bbe118dbf89ed
andreevskiy <a.andreevskiy@crystals.ru>
2019-06-17 12:36:02 +0300

    Typo in urls.py

--- irproject/ircontrol/urls.py
+++ irproject/ircontrol/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r"^$", views.index, name="index"),
 	url(r"^blamelist", views.blamelist, name="blamelist"),
-	url(r"^execute/(?P<id>[0-9]?)$", views.execute_command, name="execute"),
+	url(r"^execute/(?P<id>[0-9]+)$", views.execute_command, name="execute"),
 	url(r"^temp$", views.measure_temp, name="measure_temp"),
 	url(r"^temperature$", views.temperature_view, name="temperature_view"),
 ]
